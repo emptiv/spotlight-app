bd7297fc9820111130814e1346c8eea8ae197f64
emptiv <143311588+mt370@users.noreply.github.com>
2025-05-30 09:09:08 +0800

    basics

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { styles } from "../styles/auth.styles";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen. 123</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+      
+      <Image source={{uri: "https://images.unsplash.com/photo-1748228885250-49564b614db9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}} 
+        style={{width: 100, height: 100}}/>
+
     </View>
   );
 }
