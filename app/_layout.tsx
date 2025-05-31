a40c7a0c31d6ee26b06aad6c24ec342dc689a508
emptiv <143311588+mt370@users.noreply.github.com>
2025-05-31 10:41:43 +0800

    clerk and convex integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,15 @@
-import { ClerkProvider } from '@clerk/clerk-expo';
-import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import ClerkAndConvexProvider from "../providers/ClerkAndConvexProvider";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkAndConvexProvider>
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
         <Stack screenOptions={{headerShown: false}}/>
       </SafeAreaView>
     </SafeAreaProvider>
-    </ClerkProvider>
+    </ClerkAndConvexProvider>
   );
 }
